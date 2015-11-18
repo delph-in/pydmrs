01944422cca29fdbabb288affc1de7c50dc3e4f4
guyemerson <g.e.t.emerson@googlemail.com>
2015-11-18 15:13:49 +0000

    Methods for Nodes

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ class RealPred(Pred, namedtuple('RealPred',('lemma','pos','sense'))):
     """
     def __new__(cls, lemma, pos, sense=None):
         """
-        Allow the sense to be optional
+        Create a new instance, allowing the sense to be optional
         """
         return super(RealPred, cls).__new__(cls, lemma, pos, sense)
     
@@ -82,27 +82,102 @@ class Node(object):
     """
     A superclass to set the API for all Node classes
     """
-    def __init__(self, nodeid, pred, sortinfo, cfrom, cto, carg):
+    def __init__(self, nodeid, pred, sortinfo, cfrom, cto, carg, cvtype):
         self.id = nodeid
         self.pred = pred
         self.sortinfo = sortinfo
         self.cfrom = cfrom
         self.cto = cto
-        self.span = (cfrom, cto)
         self.carg = carg
+        self.cvtype = cvtype
+    
+    @property
+    def span(self):
+        if self.cfrom and self.cto:
+            return (self.cfrom, self.cto)
+        else:
+            return None
 
 
 class ListNode(Node):
     """
     A node implemented with a list for the sortinfo
     """
-    pass
+    def dict_node(self, graph=None):
+        """
+        Convert to a DictNode
+        """
+        return DictNode(self.id, self.pred, self.sortinfo, self.cfrom, self.cto, self.carg, self.cvtype, graph)
+
 
 class DictNode(Node):
     """
-    A node implemented with a dict for the sortinfo
+    A node implemented with a dict for the sortinfo,
+    and a pointer to the DMRS graph to access links
     """
-    pass
+    def __init__(self, nodeid, pred, sortinfo=None, cfrom=None, cto=None, carg=None, cvtype=None, graph=None):
+        self.id = nodeid
+        self.pred = pred
+        self.cfrom = cfrom
+        self.cto = cto
+        self.carg = carg
+        self.cvtype = cvtype
+        self._graph = graph
+        if sortinfo:
+            if isinstance(sortinfo, list):
+                self.sortinfo = {x:y for x,y in sortinfo}
+            else:
+                assert isinstance(sortinfo, dict)
+                self.sortinfo = sortinfo
+        else:
+            self.sortinfo = {}
+    
+    def list_node(self):
+        """
+        Convert to a ListNode
+        """
+        return ListNode(self.id, self.pred, self.sortinfo.items(), self.cfrom, self.cto, self.carg, self.cvtype)
+    
+    @property
+    def incoming(self):
+        """
+        Incoming links, indexed by nodeids
+        """
+        return self._graph.incoming[self.id]
+    
+    @property
+    def outgoing(self):
+        """
+        Outgoing links, indexed by nodeids
+        """
+        return self._graph.outgoing[self.id]
+    
+    def in_label(self, label, post=None):
+        """
+        Find all incoming links with a given label
+        """
+        if post:
+            label = LinkLabel(label, post)
+        return search_keys(self.incoming, label)
+    
+    def out_label(self, label, post=None):
+        """
+        Find all outgoing links with a given label
+        """
+        if post:
+            label = LinkLabel(label, post)
+        return search_keys(self.outgoing, label)
+
+def search_keys(dictionary, target):
+    """
+    For a dictionary of sets,
+    search for keys whose values contain the target
+    """
+    output = set()
+    for key, valueset in dictionary.items():
+        if target in valueset:
+            output.add(key)
+    return output
 
 
 
@@ -142,7 +217,7 @@ class ListDmrs(Dmrs):
         """
         Convert to a DictDmrs
         """
-        return DictDmrs(self.nodes, self.links)
+        return DictDmrs(self.nodes, self.links, self.top)
 
 
 class DoubleDict(dict):
@@ -150,11 +225,19 @@ class DoubleDict(dict):
     A dict of dicts, mapping to sets.
     Used to store links in the DictDmrs class below.
     """
+    def add(self, key1, key2, value):
+        """
+        Add an entry, creating new dicts or sets as necessary,
+        i.e. self[key1][key2].add(value) with defaults
+        """
+        self.setdefault(key1,{}).setdefault(key2,set()).add(value)
+    
     def trim(self, key1, key2, value):
         """
-        Remove an entry, avoiding empty dicts or sets
+        Remove an entry, cleaning up empty dicts or sets
         """
-        assert value in self[key1][key2]
+        if not value in self[key1][key2]:
+            raise KeyError((key1, key2, value))
         n = len(self[key1][key2])
         if n > 1:
             self[key1][key2].remove(value)
@@ -167,7 +250,8 @@ class DoubleDict(dict):
         """
         Remove a whole set, not just a single element
         """
-        assert self[key1][key2]
+        if not self[key1][key2]:
+            raise KeyError((key1, key2))
         if len(self[key1]) > 1:
             self[key1].pop(key2)
         else:
@@ -181,16 +265,28 @@ class DictDmrs(Dmrs):
         """
         Initialise dictionaries from lists
         """
-        self.top = top
-        self._nodes = {n.id:n for n in nodes}
+        # Initialise nodes
+        self._nodes = {}
+        for n in nodes:
+            # Allow the list of nodes to be DictNodes, ListNodes, or tuples
+            if isinstance(n, DictNode):
+                self._nodes[n.id] = n
+                n.graph = self
+            elif isinstance(n, ListNode):
+                self._nodes[n.id] = n.dict_node(graph=self)
+            else:
+                self._nodes[n[0]] = DictNode(*n, graph=self)
+        # Initialise links
         self.outgoing = DoubleDict()
         self.incoming = DoubleDict()
         for start, end, rargname, post in links:
-            assert start in self
-            assert end in self
             label = LinkLabel(rargname, post)
-            self.outgoing.setdefault(start,{}).setdefault(end,set()).add(label)
-            self.incoming.setdefault(end,{}).setdefault(start,set()).add(label)
+            self.add_link(start, end, label)
+        # Allow top to be a nodeid, or a node
+        if top in self:
+            self.top = self[top]
+        else:
+            assert top.id in self
     
     def __getitem__(self, nodeid):
         """
@@ -215,10 +311,31 @@ class DictDmrs(Dmrs):
     def nodes(self):
         return list(self._nodes.values())
     
+    def add_link(self, start, end, label, post=None):
+        """
+        Safely add a link.
+        If (start, end, label) is given, adds a link from start to end with that label
+        If (start, end, rargname, post) is given, the label is (rargname, post)
+        """
+        if not (start in self and end in self):
+            raise KeyError((start, end))
+        if post:
+            rargname = label
+            label = LinkLabel(rargname, post)
+        self.outgoing.add(start, end, label)
+        self.incoming.add(end, start, label)
+    
+    def add_links(self, iterable):
+        """
+        Safely add a number of links
+        """
+        for link in iterable:
+            self.add_link(*link)
+    
     def remove_link(self, start, end, label=None, post=None):
         """
         Safely remove a link.
-        If (start, end) are given, removes all links from start to end
+        If (start, end) is given, removes all links from start to end
         If (start, end, label) is given, removes the link from start to end with that label
         If (start, end, rargname, post) is given, removes the link with label (rargname, post)
         """
@@ -243,4 +360,5 @@ class DictDmrs(Dmrs):
         """
         Convert to a ListDmrs
         """
-        return ListDmrs(self.nodes, self.links)
+        list_nodes = [n.list_node() for n in self._nodes.values()]
+        return ListDmrs(list_nodes, self.links, self.top.id)
