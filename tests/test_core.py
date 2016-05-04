39e30fbefa0cef5817704c41839192f2ce0dbc2c
Ewa Muszynska <emm68@cam.ac.uk>
2016-05-04 11:43:46 +0100

    Node tests.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,4 @@
-import unittest, warnings
+import unittest
 
 from pydmrs.core import (
     Link, LinkLabel,
@@ -15,7 +15,7 @@ class TestLink(unittest.TestCase):
     """
     Test methods of Link and LinkLabel classes
     """
-    
+
     def test_Link_new(self):
         """
         Links should have exactly four slots (start, end, rargname, post).
@@ -43,14 +43,14 @@ class TestLink(unittest.TestCase):
             warnings.simplefilter('error')
             Link(0, 0, 1, 2)
         warnings.resetwarnings()
-    
+
     # Helper function for test_Link_new
     def assert_ex_link(self, link):
         self.assertEqual(link.start, 0)
         self.assertEqual(link.end, 1)
         self.assertEqual(link.rargname, 'RSTR')
         self.assertEqual(link.post, 'H')
-    
+
     def test_Link_str(self):
         """
         The 'informal' string representation of a Link
@@ -58,7 +58,7 @@ class TestLink(unittest.TestCase):
         """
         link = Link(0, 1, 'RSTR', 'H')
         self.assertEqual(str(link), "(0 - RSTR/H -> 1)")
-    
+
     def test_Link_repr(self):
         """
         The 'official' string representation of a Link
@@ -66,7 +66,7 @@ class TestLink(unittest.TestCase):
         """
         link = Link(0, 1, 'RSTR', 'H')
         self.assertEqual(link, eval(repr(link)))
-    
+
     def test_Link_label(self):
         """
         The label of a link should be a LinkLabel
@@ -75,7 +75,7 @@ class TestLink(unittest.TestCase):
         label = LinkLabel('RSTR', 'H')
         self.assertIsInstance(link.label, LinkLabel)
         self.assertEqual(link.label, label)
-    
+
     def test_Link_labelstring(self):
         """
         The labelstring of a link should be its label's string 
@@ -83,7 +83,7 @@ class TestLink(unittest.TestCase):
         link = Link(0, 1, 'RSTR', 'H')
         labelstring = 'RSTR/H'
         self.assertEqual(link.labelstring, labelstring)
-    
+
     def test_Link_copy(self):
         """
         copy.copy should return an equal Link
@@ -100,7 +100,7 @@ class TestLink(unittest.TestCase):
         # Note that it doesn't make sense to check
         # if link.end is not link_deep.end,
         # because identical strings and ints are considered to be the same
-    
+
     def test_LinkLabel_new(self):
         """
         LinkLabels should have exactly two slots (rargname, post).
@@ -110,18 +110,18 @@ class TestLink(unittest.TestCase):
         # Check two arguments
         self.assert_rstr_h(LinkLabel('RSTR', 'H'))
         self.assert_rstr_h(LinkLabel(rargname='RSTR', post='H'))
-        
+
         # Check wrong numbers of arguments
         with self.assertRaises(TypeError):
             LinkLabel(0, 1, 2)
         with self.assertRaises(TypeError):
             LinkLabel(0, 1, 2, 3, 4)
-    
+
     # Helper function for test_LinkLabel_new
     def assert_rstr_h(self, linklabel):
         self.assertEqual(linklabel.rargname, 'RSTR')
         self.assertEqual(linklabel.post, 'H')
-    
+
     def test_LinkLabel_str(self):
         """
         The 'informal' string representation of a LinkLabel
@@ -129,7 +129,7 @@ class TestLink(unittest.TestCase):
         """
         label = LinkLabel('RSTR', 'H')
         self.assertEqual(str(label), "RSTR/H")
-    
+
     def test_LinkLabel_repr(self):
         """
         The 'official' string representation of a LinkLabel
@@ -137,7 +137,7 @@ class TestLink(unittest.TestCase):
         """
         label = LinkLabel('RSTR', 'H')
         self.assertEqual(label, eval(repr(label)))
-    
+
     def test_LinkLabel_copy(self):
         """
         copy.copy should return an equal LinkLabel
@@ -160,6 +160,13 @@ class TestNode(unittest.TestCase):
     """
     Test methods for Node class.
     """
+
+    def test_Node_str(self):
+        node = Node()
+        self.assertEqual(str(node), "None")
+        node = Node(nodeid=2, pred='_dog_n_1', sortinfo=dict(cvarsort='i', pers='3', num='sg', ind='+'), carg='Pat')
+        self.assertEqual(str(node), '_dog_n_1(Pat) x[pers=3, num=sg, ind=+]')
+
     def test_Node_eq(self):
         # Unspecified nodes are always equal.
         node1 = Node()
@@ -170,7 +177,7 @@ class TestNode(unittest.TestCase):
         sortinfo2 = {'cvarsort': 'e', 'tense': 'pres'}
 
         # Two nodes are equal if they have the same pred, sortinfo and carg, even if all the other elements are different
-        node1 = Node(nodeid = 23, pred='the_q', sortinfo=sortinfo1, cfrom=2, cto=22, carg='Kim', surface='cat', base='x')
+        node1 = Node(nodeid=23, pred='the_q', sortinfo=sortinfo1, cfrom=2, cto=22, carg='Kim', surface='cat', base='x')
         node2 = Node(nodeid=25, pred='the_q', sortinfo=sortinfo1, cfrom=15, carg='Kim', surface='mad', base='w')
         self.assertEqual(node1, node2)
 
@@ -185,3 +192,19 @@ class TestNode(unittest.TestCase):
         # Different sortinfo.
         node2 = Node(pred='_the_q', sortinfo=sortinfo2, carg='Kim')
         self.assertNotEqual(node1, node2)
+
+    def test_Node_leq(self):
+        with self.assertRaises(TypeError):
+            4 < Node(pred='the_q')
+
+    def test_Node_span(self):
+        node = Node(cfrom=2, cto=15)
+        self.assertEqual(node.span, (2, 15))
+
+    def test_Node_isgpred_realpred_node(self):
+        gnode = Node(pred='the_q')
+        realnode = Node(pred='_cat_n')
+        self.assertTrue(gnode.is_gpred_node)
+        self.assertTrue(realnode.is_realpred_node)
+        self.assertFalse(gnode.is_realpred_node)
+        self.assertFalse(realnode.is_gpred_node)
