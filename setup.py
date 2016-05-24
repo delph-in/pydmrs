95b08e9723ecd6baa34c19312d3e608edd9e50c5
guyemerson <g.e.t.emerson@googlemail.com>
2016-05-24 17:39:57 +0200

    Prepare for release

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pydmrs',
+  packages = ['pydmrs'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'A library for manipulating DMRS graphs',
+  author = 'Ann Copestake, Guy Emerson, Mike Goodman, Matic Horvat, Alex Kuhnle, Ewa Muszyńska',
+  author_email = 'gete2@cam.ac.uk',
+  url = 'https://github.com/delph-in/pydmrs',
+  download_url = 'https://github.com/delph-in/pydmrs/tarball/1.0.0',
+  keywords = ['NLP', 'Natural Language Processing', 'Computational Linguistics', 'Semantics'],
+  classifiers = [],
+)
