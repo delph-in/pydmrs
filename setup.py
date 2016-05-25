3c24b2f979939f46bb8de74a2aa90f2cef60ec11
guyemerson <g.e.t.emerson@googlemail.com>
2016-05-25 16:17:44 +0200

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'pydmrs',
   packages = ['pydmrs'], # this must be the same as the name above
-  version = '1.0.0',
+  version = '1.0.1',
   description = 'A library for manipulating DMRS graphs',
   author = 'Ann Copestake, Guy Emerson, Mike Goodman, Matic Horvat, Alex Kuhnle, Ewa Muszyńska',
   author_email = 'gete2@cam.ac.uk',
   license = 'MIT',
   url = 'https://github.com/delph-in/pydmrs',
-  download_url = 'https://github.com/delph-in/pydmrs/tarball/1.0.0',
+  download_url = 'https://github.com/delph-in/pydmrs/tarball/1.0.1',
   keywords = ['NLP', 'Natural Language Processing', 'Computational Linguistics', 'Semantics'],
   install_requires = [
     'pydelphin'
