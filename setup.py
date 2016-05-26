4982197055ad740faabf06c8eeabba1145862f40
guyemerson <g.e.t.emerson@googlemail.com>
2016-05-26 10:04:41 +0200

    Include config files pypi package

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+VERSION = '1.0.2'
+
 setup(
   name = 'pydmrs',
-  packages = ['pydmrs'], # this must be the same as the name above
-  version = '1.0.1',
+  version = VERSION,
   description = 'A library for manipulating DMRS graphs',
   author = 'Ann Copestake, Guy Emerson, Mike Goodman, Matic Horvat, Alex Kuhnle, Ewa Muszyńska',
   author_email = 'gete2@cam.ac.uk',
   license = 'MIT',
   url = 'https://github.com/delph-in/pydmrs',
-  download_url = 'https://github.com/delph-in/pydmrs/tarball/1.0.1',
+  download_url = 'https://github.com/delph-in/pydmrs/tarball/'+VERSION,
   keywords = ['NLP', 'Natural Language Processing', 'Computational Linguistics', 'Semantics'],
+  packages = find_packages().append('examples'),
+  data_files = [('configs', ['configs/'+filename for filename in os.listdir('configs')])],
   install_requires = [
     'pydelphin'
   ]
