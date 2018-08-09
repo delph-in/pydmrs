115425b182bc932fa9b0cefc93b8564300217e1a
Ewa Muszynska <emm68@cam.ac.uk>
2018-08-09 12:17:04 +0100

    Add an option to check for node equality with underspecification.

--- pydmrs/matching/common.py
+++ pydmrs/matching/common.py
@@ -1,21 +1,26 @@
-def are_equal_nodes(n1, n2):
-    """Returns True if nodes n1 and n2 have the same predicate and sortinfo."""
+def are_equal_nodes(n1, n2, underspecified=True):
+    """Returns True if nodes n1 and n2 have the same predicate and sortinfo. If underspecified,
+    allow underspecification."""
+    if underspecified:
+        if n1.is_less_specific(n2) or n2.is_less_specific(n1):
+            return True
     return n1.pred == n2.pred and n1.sortinfo == n2.sortinfo and n1.carg == n2.carg
 
 
-def are_equal_links(l1, l2, dmrs1, dmrs2):
+def are_equal_links(l1, l2, dmrs1, dmrs2, underspecified=True):
     """Returns True if links l1 and l2 have the same link label and their
        starting and ending nodes respectively satisfy are_equal_nodes."""
     if l1.label == l2.label:
         if l1.rargname is None:
-            if (are_equal_nodes(dmrs1[l1.start], dmrs2[l2.start]) and
-                    are_equal_nodes(dmrs1[l1.end], dmrs2[l2.end])) or (are_equal_nodes(dmrs1[l1.start], dmrs2[l2.end])
-                                                                       and are_equal_nodes(dmrs1[l1.end],
-                                                                                           dmrs2[l2.start])):
+            if (are_equal_nodes(dmrs1[l1.start], dmrs2[l2.start], underspecified) and
+                    are_equal_nodes(dmrs1[l1.end], dmrs2[l2.end], underspecified)) or (
+                        are_equal_nodes(dmrs1[l1.start], dmrs2[l2.end], underspecified)
+                    and are_equal_nodes(dmrs1[l1.end],
+                                        dmrs2[l2.start], underspecified)):
                 return True
         else:
-            if (are_equal_nodes(dmrs1[l1.start], dmrs2[l2.start]) and
-                    are_equal_nodes(dmrs1[l1.end], dmrs2[l2.end])):
+            if (are_equal_nodes(dmrs1[l1.start], dmrs2[l2.start], underspecified) and
+                    are_equal_nodes(dmrs1[l1.end], dmrs2[l2.end], underspecified)):
                 return True
     else:
         return False
